b827500eeb793bc39d57ef6f0d6e0bf32472af11
mbudak <promaxcoq@gmail.com>
2022-05-26 16:34:19 -0400

    Done first Prototype

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// your code
+// elements
+var btn_AddRow = document.getElementById('add-row');
+var pixelTable = document.getElementById('pixelTable');
+var currentColor = document.getElementById('currentColor');
+// Events 
+btn_AddRow.addEventListener('click', makeRow);
+
+function colorize() {
+    this.setAttribute('class', currentColor.value);    
+}
+
+
+function makeRow(colCount) {
+    let row = pixelTable.insertRow(0);
+    for (let i = 0; i < colCount; i++) {
+        let cell = row.insertCell(0);
+        cell.addEventListener('click', colorize, true);        
+    }
+}
+
+// add a matrix
+function AddTable(row, col) {
+    for (let i = 0; i < 20; i++) {
+        makeRow(20);
+    }
+}
+
+
+// Ready to Run
+if (true) {
+    // add a 20x20 matrix
+    AddTable(20, 20);
+}
