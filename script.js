4e13a08118671a8f216861d7d102208f56713d5c
mbudak <promaxcoq@gmail.com>
2022-05-26 17:26:28 -0400

    fill only white cells

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 // buttons
 var btn_AddRow = document.getElementById('add-row');
 var btn_FillAll = document.getElementById('fillAll');
+var btn_FillWhites = document.getElementById('fillWhites');
+
 // other elements
 var pixelTable = document.getElementById('pixelTable');
 var currentColor = document.getElementById('currentColor');
 var drawByMouseOn = document.getElementById('drawByMouseOn');
 
 
-// Events 
-btn_AddRow.addEventListener('click', makeRow);
+// Add Events to Elements
+btn_AddRow.addEventListener('click', makeRow(20));
 btn_FillAll.addEventListener('click', fillAll);
+btn_FillWhites.addEventListener('click', fillWhites);
+
+
+// Event Functions
 function colorize() {
     this.setAttribute('class', currentColor.value);    
 }
@@ -25,14 +31,25 @@ function fillAll() {
     tdList.forEach(element => {
         element.setAttribute('class', currentColor.value);
     });
-
 }
+
+function fillWhites() {
+    var tdList = document.querySelectorAll("#pixelTable td");
+    tdList.forEach(element => {
+        if (element.classList.contains("white")) {
+            element.setAttribute('class', currentColor.value);
+        }
+    });
+}
+
+
 
 /* Table Functions */
 function makeRow(colCount) {
     let row = pixelTable.insertRow(0);
     for (let i = 0; i < colCount; i++) {
         let cell = row.insertCell(0);
+        cell.setAttribute('class', "white"); // Default class
         cell.addEventListener('click', colorize);
         cell.addEventListener('mouseover', colorizeByMouse);       
     }
